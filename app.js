a87cf6d26394ea3b1c2427e4707ba9e09946c55c
rrmctapia <rrmc.tapia@yahoo.com>
2021-07-14 11:45:36 -0700

    styling/editing

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ document.getElementById("noAcc").addEventListener('click',signUp);
 function signUp(){
     if(document.getElementById("confirmPass").style.display === "none"){
         document.getElementById("confirmPass").style.display ="block";
-        document.getElementById('noAcc').innerHTML = "Already have an account? Log in.";
+        document.getElementById('noAcc').innerHTML = "Already have an account? <strong> Log in. </strong>";
+        document.getElementById('login-button').innerHTML = " Sign Up";
     } else {
         document.getElementById("confirmPass").style.display ="none";
-        document.getElementById('noAcc').innerHTML = "Don't have an account? Sign Up.";
+        document.getElementById('noAcc').innerHTML = "Don't have an account? <strong> Sign Up.</strong>";
+        document.getElementById('login-button').innerHTML = " Log in";
     }
 }
